ce4b3796d5c1fa867080d8633893ebb42f294aba
Yinxin Gao <yinxin.gao@rakuten.com>
2020-11-25 05:01:40 +0900

    use local spark master

--- src/main/scala/SparkTest.scala
+++ src/main/scala/SparkTest.scala
@@ -2,10 +2,8 @@ import org.apache.spark.sql.SparkSession
 
 object SparkTest extends App {
   val spark = SparkSession.builder
-    .master("local[*]")
     .appName("Sample App")
-    .config("spark.driver.host","127.0.0.1")
-    .config("spark.driver.bindAddress","127.0.0.1")
+    .master("spark://127.0.0.1:7077")
     .getOrCreate()
   val data = spark.sparkContext.parallelize(
     Seq("I like Spark", "Spark is awesome", "My first Spark job is working now and is counting down these words")
